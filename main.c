82183492572531726b0a64fb7cd108b968d18507
Salomao Silva <salomaohenrque08@gmail.com>
2023-08-20 01:10:38 -0300

    Atualizando projeto de pontuacao

--- main.c
+++ main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include "celeiro.h"
+#include "celeiro.h"
+#include "pontuacao.h"
 
 char letra1 = '1';
 int main()
 {
     do{
         fflush(stdin);
+        //system("cls");
         if (kbhit()){
             letra1 = _getch();
-
             switch(letra1){
                 case 'w':
                     printf("\nAndando para cima\n\n");
@@ -24,15 +25,45 @@ int main()
                 case 'a':
                     printf("\nAndando para esquerda\n\n");
                     break;
+                case '1':
+                    printf("\nAdicionando pontos\n\n");
+                    adicionaPontos();
+                    break;
+                case '2':
+                    printf("\nAdicionando pontos com bonus\n\n");
+                    adicionaPontosBonus();
+                    break;
+                case '3':
+                    printf("\Removendo pontos\n\n");
+                    removePontos();
+                    break;
+                case '4':
+                    printf("\nAndando para esquerda\n\n");
+                    break;
+                case '5':
+                    printf("\nAndando para esquerda\n\n");
+                    break;
+                case '6':
+                    printf("\nAndando para esquerda\n\n");
+                    break;
+                case '7':
+                    printf("\nAndando para esquerda\n\n");
+                    break;
+                case '8':
+                    printf("\nAndando para esquerda\n\n");
+                    break;
+                case '9':
+                    printf("\nAndando para esquerda\n\n");
+                    break;
                 default:
                     printf("\nComando invallido!!!\n\n");
                     break;
             }
-            //printf("\n%c\n\n", letra1);
+            printf("\n%d\n\n", retornaPontuacao());
             while (_kbhit()) _getch();
         }
         else{
-            printf("Nada inserido!!!\n");
+            printf("Pontos: %d --- Nada inserido!!!\n", retornaPontuacao());
             usleep(500000);
         }
     } while (letra1 != '0');
--- pontuacao.h
+++ pontuacao.h
@@ -1,1 +1,19 @@
+int pontuacao = 0;
 
+#define PONTOS 10
+
+void adicionaPontos(){
+    pontuacao += PONTOS;
+}
+
+void adicionaPontosBonus(){
+    pontuacao += PONTOS * 5;
+}
+
+void removePontos(){
+    pontuacao -= PONTOS;
+}
+
+int retornaPontuacao(){
+    return pontuacao;
+}
